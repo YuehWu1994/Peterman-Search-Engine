aba18c29d3472371eafb8f8452c52f772eb7b674
YuehWu1994 <yuehw@uci.edu>
2019-05-30 14:25:45 -0700

    add another testcase

--- src/test/java/edu/uci/ics/cs221/index/ranking/Team12SearchTfIdf.java
+++ src/test/java/edu/uci/ics/cs221/index/ranking/Team12SearchTfIdf.java
@@ -4,10 +4,8 @@ import edu.uci.ics.cs221.analysis.Analyzer;
 import edu.uci.ics.cs221.analysis.ComposableAnalyzer;
 import edu.uci.ics.cs221.analysis.PorterStemmer;
 import edu.uci.ics.cs221.analysis.PunctuationTokenizer;
-import edu.uci.ics.cs221.index.inverted.InvertedIndexManager;
-import edu.uci.ics.cs221.index.inverted.PageFileChannel;
+import edu.uci.ics.cs221.index.inverted.*;
 import edu.uci.ics.cs221.storage.Document;
-import edu.uci.ics.cs221.index.inverted.Pair;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -21,8 +19,14 @@ import java.util.List;
 public class Team12SearchTfIdf {
     private Analyzer analyzer = new ComposableAnalyzer(new PunctuationTokenizer(), new PorterStemmer());
     private String path = "./index/Team12SearchTfIdfTest/";
+    private Compressor compressor = new DeltaVarLenCompressor();
     private InvertedIndexManager invertedIndex;
 
+    
+    private Document doc1 = new Document("The University of California, Irvine is a public research university located in Irvine, California");
+    private Document doc2 = new Document("Irvine University offers 87 undergraduate degrees and 129 graduate and professional degrees");
+    private Document doc3 = new Document("Irvine Company earns a lot of money");
+    private Document doc4 = new Document("2019 Mercedes-Benz UCI Mountain Bike World Cup");
 
     /**
      * tests if the inverted index is not a positional index should throw an UnsupportedOperationException
@@ -30,10 +34,7 @@ public class Team12SearchTfIdf {
     @Test(expected = UnsupportedOperationException.class)
     public void test1() {
         invertedIndex = InvertedIndexManager.createOrOpen(path, analyzer);
-        Document doc1 = new Document("The University of California, Irvine is a public research university located in Irvine, California");
-        Document doc2 = new Document("Irvine University offers 87 undergraduate degrees and 129 graduate and professional degrees");
-        Document doc3 = new Document("Irvine Company earns a lot of money");
-        Document doc4 = new Document("2019 Mercedes-Benz UCI Mountain Bike World Cup");
+
         invertedIndex.addDocument(doc1);
         invertedIndex.addDocument(doc2);
         invertedIndex.addDocument(doc3);
@@ -46,6 +47,33 @@ public class Team12SearchTfIdf {
         phrase.add("California");
         phrase.add("Irvine");
         Iterator<Pair<Document, Double>> iterate = invertedIndex.searchTfIdf(phrase, 2);
+    }
+
+
+    @Test
+    public void test2() {
+        invertedIndex = InvertedIndexManager.createOrOpenPositional(path, analyzer, compressor);
+
+        invertedIndex.addDocument(doc1);
+        invertedIndex.addDocument(doc2);
+        invertedIndex.addDocument(doc3);
+        invertedIndex.addDocument(doc4);
+        invertedIndex.flush();
+
+        List<String> phrase = new ArrayList<>();
+        phrase.add("University");
+        phrase.add("of");
+        phrase.add("California");
+        phrase.add("Irvine");
+        Iterator<Pair<Document, Double>> iterate = invertedIndex.searchTfIdf(phrase, 4);
+
+        Double prev = 1.0;
+
+        while (iterate.hasNext()) {
+            Double val = iterate.next().getRight();
+            assert prev > val;
+            prev = val;
+        }
     }
 
 
